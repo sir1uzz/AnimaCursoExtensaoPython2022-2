3b2a2acc2312409066c2c3a9034c4c554eeef935
Matheus Damasceno <108288409+sir1uzz@users.noreply.github.com>
2022-10-20 01:18:11 +0000

    Aprendi 3 modos de exibição

--- main.py
+++ main.py
@@ -11,7 +11,10 @@ idade = 21
 # Exibir o nome
 print (nome)
 # Exibir a idade
-print (idade)
+print ("Meu nome é "+ nome)
+print ("Minha idade é " + str(idade)+" anos")
+print (f"Minha idade é {idade}")
+print ("Minha idade é {}".format(idade))
 
 '''
 Comentário em bloco
