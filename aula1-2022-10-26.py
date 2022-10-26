2f02cec6314099535bee11221c792f18e13149bb
Matheus Damasceno <108288409+sir1uzz@users.noreply.github.com>
2022-10-26 23:15:08 +0000

    Input do usuário adicionado

---
+++ aula1-2022-10-26.py
@@ -0,0 +1,9 @@
+# comando input (): quero permitir que o usuário digite algo
+nome = input("Digite seu nome: ")
+# Variável idade
+idade = int (input ("Legal! Digite sua idade, " + nome + ": "))
+dobroidade = idade * 2
+
+# comando de saída
+print ("Seu nome é " + nome + " e sua idade é " + str(idade) + " anos")
+print ("O dobro da sua idade é " + str(dobroidade))
