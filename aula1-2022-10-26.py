1289c5321f01c7355627d37b19bf17581ea005ce
Matheus Damasceno <108288409+sir1uzz@users.noreply.github.com>
2022-10-27 00:34:33 +0000

    Estruturas condicionais adicionadas

--- aula1-2022-10-26.py
+++ aula1-2022-10-26.py
@@ -2,8 +2,22 @@
 nome = input("Digite seu nome: ")
 # Variável idade
 idade = int (input ("Legal! Digite sua idade, " + nome + ": "))
+genero = input ("Qual o seu gênero? \n M - Masculino \n F - Feminino \n")
 dobroidade = idade * 2
 
 # comando de saída
 print ("Seu nome é " + nome + " e sua idade é " + str(idade) + " anos")
 print ("O dobro da sua idade é " + str(dobroidade))
+
+# Estrutura condicional (if)
+# Se a pessoa for maior de idade, mostre "Você é maior de idade, ótimo! Já pode beber ou dirigir (e ser preso)"
+
+if (idade >= 18):
+  print("Você é maior de idade, ótimo! Já pode beber ou dirigir (e ser preso)")
+else:
+  print("Você é menor de idade :0")
+
+if (idade >= 18 and genero == "M"):
+  print("Você está apto ao alistamento")
+
+
