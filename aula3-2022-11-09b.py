e8512308257b87de889aa43c082e088c183d1aa9
Matheus Damasceno <108288409+sir1uzz@users.noreply.github.com>
2022-11-10 01:18:22 +0000

    Funções com parâmetros padrões

--- aula3-2022-11-09b.py
+++ aula3-2022-11-09b.py
@@ -17,8 +17,8 @@ def calcular_imposto(preco_produto):
 
 # Aqui é o uso
 preco = 299
-imposto = calcular_imposto(preco)
-print(imposto)
+imposto1 = calcular_imposto(preco)
+print(imposto1)
 
 # Explicação de variável local x global
 print(preco)
@@ -29,3 +29,22 @@ valores = [1.99, 24.50, 78.27, 1515.5]
 
 for valor in valores:
   print (f"O imposto de {valor} é {calcular_imposto(valor)}")
+
+print("---------------")
+# Declara um função calcula_imposto_aliquota que recebe dois parâmetros: o preço do produto e a alíquota de imposto a ser aplicada e retorna o imposto calculado. Se a aliquota não for informada, utiliza 7% como padrão.
+
+def calcular_imposto_aliquota(valor, aliquota=7):
+  imposto = valor * aliquota / 100
+  return imposto
+
+for valor in valores:
+  print(f"O imposto de {valor} é {calcular_imposto_aliquota(valor)}")
+
+print("---------------")
+for valor in valores:
+  print(f"O imposto de {valor} é {calcular_imposto_aliquota(valor, 7)}")
+
+print("---------------")
+# E se o imposto for 10?
+for valor in valores:
+  print(f"O imposto de {valor} é {calcular_imposto_aliquota(valor, 10)}")
